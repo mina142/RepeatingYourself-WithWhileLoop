93d199d590d96724459c51a4d689f15a8a64ea8a
mina142 <mina_salehi53@yahoo.com>
2019-10-23 14:11:19 -0400

    Repeating yourself with a while loop

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int secretNumber, guess = 0;
+
+        secretNumber = 5;
+
+        System.out.println("I'm thinking of a number between 1 and 1000");
+        System.out.print("Enter the number:");
+        guess = keyboard.nextInt();
+
+        while (guess != secretNumber) {
+            System.out.println("\nYou are wrong. Try again.");
+            System.out.println("Enter the number: ");
+            guess = keyboard.nextInt();
+        }
+
+        System.out.println("You are correct. You win a prize!");
+        keyboard.close();
+
+    }
+}
